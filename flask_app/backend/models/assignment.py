4e28ecd2d5e18d69331e4d5787d1b51b5b3cd896
Anshuman Suri <anshuman14021@iiitd.ac.in>
2020-11-10 22:49:19 -0500

    Add database file to branch

--- flask_app/backend/models/assignment.py
+++ flask_app/backend/models/assignment.py
@@ -5,8 +5,9 @@ class Assignment:
     def __init__(self, data_list, resource_attachments):
         self.title       = data_list[0]
         self.description = data_list[1]
-        # Replace small am/pm with capital AM/PM
-        self.due_date    = datetime.strptime(data_list[2], "%b %d, %Y %I:%M %p")
+        print(data_list[2])
+        self.due_date    = datetime.strptime(data_list[2].strip(), "%b %d, %Y %I:%M %p")
+        print(self.due_date)
         self.max_score   = data_list[3] if data_list[3] != "" else None
         self.upload_link = data_list[4]
         self.attachments = data_list[5] if type(data_list[5]) == str else ""
